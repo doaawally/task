1c4be29b5ad99ddd28d208040b19953c7e98775b
doaawally <doaahassanwally@gmail.com>
2023-03-13 16:14:15 +0200

    addtask

--- task3.js
+++ task3.js
@@ -23,3 +23,88 @@ function percentage(num1,num2,num5,num4,num3){
 }
 let final=percentage( num1,num2,num5,num4,num3 )
 console.log ( final, '%')
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////task one ////////////////////////
+
+
+
+var num1 = prompt("put your number","0")
+var num2 = prompt ("your second number","0")
+
+if((num1)>(num2)) 
+{ 
+console.log("The larger of " +num1+ " and "+ num2+ " is "+ num1+ ".");
+}   
+else
+if((num2)>(num1)) 
+{
+console.log("The max of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+}                  
+else
+{
+ console.log("The values "+ num1+ " and "+num2+ " are equal.");
+}
+
+
+
+
+
+
+/////////////task2/////////////
+
+
+
+
+// var num1 = prompt("put your number","0")
+// var num2 = prompt ("your second number","0")
+// var num3 = prompt ("your third number","0")
+// function max_of_three(num1,num2,num3) 
+//  {
+//   max_val = 0;
+//   if (num1 > num2)
+//   {
+//     max_val =num1;
+//   } else
+//   {
+//     max_val = num2;
+//   }
+//   if (num3 > max_val) 
+//   {
+//     max_val = num3;
+//   }
+//   return max_val;
+// }
+
+// console.log(max_of_three(num1 ,num2 ,num3));
+
+// function min_of_three(num1,num2,num3) 
+//  {
+//   min_val = 0;
+//   if (num1 < num2)
+//   {
+//     min_val =num1;
+//   } else
+//   {
+//     max_val = num2;
+//   }
+//   if (num3 < min_val) 
+//   {
+//     max_val = num3;
+//   }
+//   return min_val;
+// }
+// console.log(min_of_three(num1 ,num2,num3));
+
+
+
+
